e48038fee14c6f28ada1a26025d694b2450bcb20
Helder Pedro <pedrolance@gmail.com>
2021-03-23 18:40:23 +0000

    added final elements to the app

--- dna-app.py
+++ dna-app.py
@@ -58,3 +58,33 @@ X_label = list(X)
 X_values = list(X.values())
 
 X
+
+### 2. Print text
+
+st.subheader('2. Print text')
+st.write('There are  ' + str(X['A']) + ' adenine (A)')
+st.write('There are  ' + str(X['T']) + ' thymine (T)')
+st.write('There are  ' + str(X['G']) + ' guanine (G)')
+st.write('There are  ' + str(X['C']) + ' cytosine (C)')
+
+### 3. Display Dataframe
+
+st.subheader('3. Display DataFrame')
+df = pd.DataFrame.from_dict(X, orient='index')
+df = df.rename({0: 'count'}, axis='columns')
+df.reset_index(inplace=True)
+df = df.rename(columns= {'index':'nucleotide'})
+st.write(df)
+
+### 4. Display Bar Chart
+
+st.subheader('4. Display Bar Chart')
+p = alt.Chart(df).mark_bar().encode(
+    x='nucleotide',
+    y='count'
+)
+p = p.properties(
+    width=alt.Step(80)
+)
+
+st.write(p)
